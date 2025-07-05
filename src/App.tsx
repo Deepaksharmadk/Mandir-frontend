0a84f4bd154b5404f8e197c4fdd98dd67d683c8a
Deepaksharmadk <deepakdbg@gmail.com>
2025-07-05 16:05:15 +0530

    Add loading state with spinner in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Layout } from './component/Layout/Layout'
 import { CarouselDemo } from './component/Carousel/CarouselComponent'
 import { AuthenticationForm } from './component/Auth/GooogleLoginwith'
+import { useGetAllImagesQuery } from './store/api/imageApi'
+import { Loader } from '@mantine/core'
 
 function App() {
+    const { isLoading } = useGetAllImagesQuery();
+    if (isLoading) {
+        return <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Loader color="orange" size="lg" type="bars" />
+        </div>
+    }
+
 
     return (
         <>
